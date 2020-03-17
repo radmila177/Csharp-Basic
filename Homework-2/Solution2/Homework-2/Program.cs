64f8b4d2d7c4472854b77538e5fbd50a33ffe2f1
radmila177 <radmila_177@hotmail.com>
2020-03-17 20:13:53 +0100

    homeworks is added

---
+++ Homework-2/Solution2/Homework-2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace RealCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter First number");
+            int firstNumber = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Please enter Second number");
+            int secondNumber = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("what operation would you want to be done: ( +, - , * , / )");
+            string operation = Console.ReadLine();
+
+            if (operation == "+")
+            {
+                Console.WriteLine("The result of sum is:" + (firstNumber + secondNumber));
+            }
+            else if (operation == "-")
+            {
+                Console.WriteLine("The result substraction is:" + (firstNumber - secondNumber));
+            }
+            else if (operation == "*")
+            {
+                Console.WriteLine("The result multiplication is:" + (firstNumber * secondNumber));
+            }
+            else if (operation == "/")
+            {
+                Console.WriteLine("The result division is:" + (firstNumber / secondNumber));
+            }
+            else
+            {
+                Console.WriteLine("You did not enter any of the offered operations");
+            }
+        }
+    }
+}
---
+++ Homework-3/Solution3/StudentGroup/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace StudentGroup
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter student group: ( there are 1 and 2 )");
+            string[] studentsG1 = new string[]
+            {
+                "Zdravko",
+                "Petko",
+                "Stanko",
+                "Branko",
+                "Trajko"
+            };
+            string[] studentsG2 = new string[] 
+            { 
+                "Radmila",
+                "Kristina",
+                "Vesna",
+                "Jovana",
+                "Elena"
+            };
+
+            var numberStudentGroup = int.Parse(Console.ReadLine());
+
+           
+                if (numberStudentGroup == 1)
+                {
+                    for (int i = 0; i < studentsG1.Length; i++)
+                    {
+                    Console.WriteLine("The Students in G1 are: " + studentsG1[i]);
+                    }
+                }
+
+                if (numberStudentGroup == 2)
+                {
+                    for (int i = 0; i < studentsG2.Length; i++)
+                    {
+                    Console.WriteLine("The Students in G2 are: " + studentsG2[i]);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("There is no such group of students");
+                }
+            }
+        }
+    }
