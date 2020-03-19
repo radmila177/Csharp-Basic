2a2215b53b4a447fae339d881116767915b1e419
radmila177 <radmila_177@hotmail.com>
2020-03-19 15:31:24 +0100

    Folder with homework is added

---
+++ Homework-2 C#/Solution2/AverageNumber/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace AverageNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter First number");
+            int firstNumber = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Please enter Second number");
+            int secondNumber = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Please enter Third number");
+            int thirdNumber = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Please enter Four number");
+            int fourNumber = int.Parse(Console.ReadLine());
+
+            var average = (firstNumber + secondNumber + thirdNumber + fourNumber) / 4;
+            Console.WriteLine("The average of " + firstNumber + "," + secondNumber + "," + thirdNumber + " and " + fourNumber + " is: " + average);
+
+        }
+    }
+    
+}
