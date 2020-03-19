8fb546e107758b5195ed973241326ef06d33d3c4
radmila177 <radmila_177@hotmail.com>
2020-03-19 15:44:07 +0100

    RealCalculator, AverageNumber, SwapNumbers

---
+++ Homework-2 C#/Solution2/SwapNumbers/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace SwapNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x;
+            Console.WriteLine("Please enter First number");
+            int firstNumber = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Please enter Second number");
+            int secondNumber = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Before swapping: " + "First number is: " + firstNumber + " Second number is: " + secondNumber);
+
+
+            x = firstNumber;
+            firstNumber = secondNumber;
+            secondNumber = x;
+
+            Console.WriteLine("After swapping: " + "First number is: " + firstNumber + " Second number is: " + x);
+        }
+    }
+    
+}
