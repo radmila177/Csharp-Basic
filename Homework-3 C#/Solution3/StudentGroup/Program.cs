b24fb301493b6c5d1a29d3f4ce628d5c90021840
radmila177 <radmila_177@hotmail.com>
2020-03-23 16:24:16 +0100

    SumOfEven, StudentGroup

---
+++ Homework-3 C#/Solution3/StudentGroup/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace StudentGroup
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter student group: ( there are 1 and 2 )");
+            string[] studentsG1 = new string[]
+            {
+                "Zdravko",
+                "Petko",
+                "Stanko",
+                "Branko",
+                "Trajko"
+            };
+            string[] studentsG2 = new string[]
+            {
+                "Radmila",
+                "Kristina",
+                "Vesna",
+                "Jovana",
+                "Elena"
+            };
+
+            var numberStudentGroup = int.Parse(Console.ReadLine());
+
+            if (numberStudentGroup == 1)
+            {
+                Console.WriteLine("The Students in G2 are: ");
+                for (int i = 0; i < studentsG1.Length; i++)
+                {
+                    Console.WriteLine(studentsG1[i]);
+                }
+            }
+
+            else if (numberStudentGroup == 2)
+            {
+                Console.WriteLine("The Students in G2 are: ");
+                for (int i = 0; i < studentsG2.Length; i++)
+                {
+                    Console.WriteLine(studentsG2[i]);
+                }
+            }
+            else
+            {
+                Console.WriteLine("There is no such group of students");
+            }
+
+
+        }
+    }
+}
