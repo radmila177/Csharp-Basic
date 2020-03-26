ce41c6b45322b2779a7dc38979de464d1dbfa9a8
radmila177 <radmila_177@hotmail.com>
2020-03-26 17:41:55 +0100

    Homework Methods

---
+++ Homework-4 C#/Solution4/Methods/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Methods
+{
+    class Program
+    {
+
+        static void Substrings()
+        {
+
+            string xString = "Hello from SEDC Codecademy v7.0";
+            Console.WriteLine("Hello from SEDC Codecademy v7.0");
+            Console.WriteLine($"Please enter one number to {xString.Length}");
+
+            bool inputNumber = int.TryParse(Console.ReadLine(), out int number);
+
+            if (!inputNumber)
+            {
+                Console.WriteLine("Please enter the number");
+            }
+            else if (number < 1 || number > xString.Length)
+            {
+                Console.WriteLine("Please enter the corect number");
+            }
+            else
+            {
+                string nCharacter = xString.Substring(0, number);
+                Console.WriteLine($"The new string is {nCharacter}.");
+                Console.WriteLine($"The lenght of new string is: {nCharacter.Length}");
+            }
+
+            Console.ReadLine();
+        }
+        static void Main(string[] args)
+        {
+            Substrings();
+        }
+    }
+}
---
+++ Homework-4 C#/Solution4/SumAndSubtract/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace SumAndSubtract
+{
+    class Program
+    {
+        static int Sum(int number1, int number2)
+        {
+            int result = number1 + number2;
+            return result;
+        }
+        static int Subtract(int number1, int number2)
+        {
+            int result1 = number1 - number2;
+            return result1;
+        }
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Please enter two numbers:");
+            bool inputNumber1 = int.TryParse(Console.ReadLine(), out int num1);
+
+            if (!inputNumber1)
+            {
+                Console.WriteLine("Please enter a number:");
+            }
+           
+            bool inputNumber2 = int.TryParse(Console.ReadLine(), out int num2);
+
+            if (!inputNumber2)
+            {
+                Console.Write("Please enter a number:");
+            }
+
+            Console.WriteLine("What operation would you want to be done: ( +, - )");
+            bool operators = char.TryParse(Console.ReadLine(), out char operation);
+
+            if (!operators)
+            {
+                Console.WriteLine("Invalid operator.Please enter a corect operator.");
+            }
+            else
+            {
+                switch (operation)
+                {
+                    case '+':
+                        Console.WriteLine($"The sum of {num1} and {num2} is {Sum(num1, num2)}");
+                        break;
+                    case '-':
+                        Console.WriteLine($"The substraction of {num1} and {num2} is {Subtract(num1, num2)}");
+                        break;
+                    default:
+                        Console.WriteLine("Invalid operation");
+                        break;
+                }
+                Console.ReadLine();
+            }
+        }
+
+    }
+}
