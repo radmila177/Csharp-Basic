80cb711be59562aa35f193e56cb9aa419ff32f26
radmila177 <radmila_177@hotmail.com>
2020-04-14 13:38:42 +0200

    workshop

---
+++ workshop c#/Workshop/CompanyLibrary/Models/CEO.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CompanyLibrary.Models
+{
+    public class CEO: Employee
+     {
+
+        public Employee[] Employees { get; set; }
+        public int Shares { get; set; }
+        private double SharesPrice { get; set; }
+
+
+        public CEO(string firstname, string lastname)
+        {
+            FirstName = firstname;
+            LastName = lastname;
+            Salary = 1500;
+            Shares = 1;
+           
+        }
+
+        public double AddSharesPrice(double sharePrice)
+        {
+            return SharesPrice = sharePrice;
+        }
+
+        public string PrintEmployees()
+        {
+            return $"Employees: {Employees}";
+        }
+
+        public override double GetSalary()
+        {
+            return Salary += Shares * SharesPrice;
+        }
+
+
+    }
+}
---
+++ workshop c#/Workshop/CompanyLibrary/Models/Contractor.cs
@@ -0,0 +1,27 @@
+﻿using CompanyLibrary.Enum;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CompanyLibrary.Models
+{
+    public class Contractor : Employee
+    {
+        public double WorkHours { get; set; }
+        public int PayPerHour { get; set; }
+        public Manager Responsible { get; set; }
+
+        public Contractor() { }
+        public Contractor(string firstname, string lastname)
+        {
+            FirstName = firstname;
+            LastName = lastname;
+            WorkHours = 40;
+            PayPerHour = 5;
+        }
+        public override double GetSalary()
+        {
+            return Salary = WorkHours * PayPerHour;
+        }
+    }
+}
