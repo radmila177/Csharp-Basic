06179507585db04450f57d5f26d95d681463a250
radmila177 <radmila_177@hotmail.com>
2020-04-14 15:14:45 +0200

    Workshop

---
+++ workshop c#/Workshop/CompanyLibrary/Models/CEO.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CompanyLibrary.Models
+{
+    public class CEO: Employee
+     {
+
+        public Employee[] Employees { get; set; }
+        public int Shares { get; set; }
+        private double SharesPrice { get; set; }
+
+
+        public CEO(string firstname, string lastname, Employee[] empl)
+        {
+            FirstName = firstname;
+            LastName = lastname;
+            Employees = empl;
+            Salary = 1500;
+            Shares = 1;
+        }
+
+        public double AddSharesPrice(double sharePrice)
+        {
+            return SharesPrice = sharePrice;
+        }
+
+        public void PrintEmployees()
+        {
+            Console.WriteLine($"Employees: {Employees}");
+        }
+
+        public override double GetSalary()
+        {
+            return Salary += Shares * SharesPrice;
+        }
+
+
+    }
+}
