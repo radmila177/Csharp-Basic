8e640131ee582a41833fc3e34e273c701a926fa2
radmila177 <radmila_177@hotmail.com>
2020-04-15 14:09:16 +0200

    Workshop

---
+++ workshop c#/Workshop/CompanyLibrary/Enum/RoleType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CompanyLibrary.Enum
+{
+    public enum RoleType
+    {
+      Sales,
+      Menager,
+      Other
+    }
+}
---
+++ workshop c#/Workshop/CompanyLibrary/Models/CEO.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CompanyLibrary.Models
+{
+    public class CEO: Employee
+     {
+
+        public Employee[] Employees { get; set; }
+        public int Shares { get; set; }
+        private double SharesPrice { get; set; }
+
+
+        public CEO(string firstname, string lastname, Employee[] empl)
+        {
+            FirstName = firstname;
+            LastName = lastname;
+            Employees = empl;
+            Salary = 1500;
+            Shares = 1;
+        }
+
+        public void AddSharesPrice(double sharePrice)
+        {
+            SharesPrice = sharePrice;
+        }
+
+        public void PrintEmployees(Employee[] Employees)
+        {
+            Console.WriteLine($"Employees: ");
+
+            foreach (var value in Employees)
+            {
+                Console.WriteLine($"{value.FirstName} {value.LastName}");
+            }
+            
+        }
+        public override double GetSalary()
+        {
+            return Salary += Shares * SharesPrice;
+        }
+
+
+    }
+}
